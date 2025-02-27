8b4dab04900353c6fe8b4d233400903d4f52e9fe
Nayttt-14 <nglruferos22@src.pshs.edu.ph>
2025-02-27 10:04:24 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var fiveLetterWords = ["Brick", "Shout", "Plumb", "Frost", "Glint", "Charp", "Sigma", "Bonds", "Quirk", "Jumps", "Vexil", "Graph", "Tonic", "Dwarf", "Blush", "Midge", "Freaky", "Flute", "Hyped", "Cramp", "Zesty", "Nymph", "Joked", "Brisk", "Vault", "Chord", "Minor", "Funky", "Tread", "Glove", "Spurn", "Mothy", "Blend", "Quash", "Vomit", "Fable", "Trick", "Douse", "Grasp", "Juicy", "Exalt", "Clamp", "Sworn", "Tacky", "Bored", "Guilt", "Shave", "Drown", "Faint", "Jolts"];
+var wordArray = Math.round((Math.random() * 100) / 2) - 1;
+var wordToGuess = fiveLetterWords[wordArray];
+var trials = 5;
+var stickmanImage= 1;
